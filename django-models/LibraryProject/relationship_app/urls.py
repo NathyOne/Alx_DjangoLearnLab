85262460389004388853aef3bedd069496d4bd14
Nathaniel <nathanielabebe32@gmail.com>
2025-11-16 05:07:43 +0300

    auth functionality included

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.lists_books, name="index"),
     path('library/books', views.LibraryDetailView.as_view(), name=" books_in_library"),
     path('register/', views.register, name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(template_name = "login.html"), name='login'),
+    path('logout/', views.LogoutView.as_view(template_nmae = 'logout.html'), name='logout'),
 ]
 
