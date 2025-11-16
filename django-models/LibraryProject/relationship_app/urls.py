aa53a456cb3b219bf940bd0617d9b61162c43a8f
Nathaniel <nathanielabebe32@gmail.com>
2025-11-16 05:04:21 +0300

    auth functionality included

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,5 +5,8 @@ from .views import list_books, LibraryDetailView
 urlpatterns = [
     path('', views.lists_books, name="index"),
     path('library/books', views.LibraryDetailView.as_view(), name=" books_in_library"),
+    path('register/', views.register, name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Book, Author, Librarian, Library
-/from .models import Library
+from .models import Library
 from django.views.generic.detail import DetailView
+from .forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm
+from django.views import View
+from django.contrib.auth import authenticate, login, logout
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
+
+
+
 # Create your views here.
 def lists_books(request):
     book_list = Book.objects.all()
@@ -11,13 +22,76 @@ def lists_books(request):
     }
     return render(request, "relationship_app/list_books.html", context=context)
 
-
-#Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
-# Utilize Django’s ListView or DetailView to structure this class-based view.
 class LibraryDetailView(DetailView):
     # all_books_in_the_Library = Library.objects.get(name="main library").books.all()
     all_books_in_library = Book.objects.filter(library__name="Main Library")
     template_name =  "relationship_app/library_detail.html"
+
+
+
+# views for user login, logout, and registration.
+
+def Registration(request):
+    if request.method=="POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "registration successfull")
+    else:
+        form = UserRegistrationForm()
+
+    
+    return render(request, 'registration/register.html', {'form': form})
+
+
+
+
+class Login(View):
+
+    template_name = 'login.html'
+    @method_decorator(csrf_protect)
+    def get(self, request):
+
+        if request.user.is_authenticated:
+            return redirect('')
+        render(request, self.template_name)
+
+
+    def post(self, request):
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+
+        user = authenticate(request, username = username, password=password)
+
+        if user is not None:
+            login(user, request)
+
+            messages.success(request, "login successfull")
+            next_page = request.GET.get('next' , '')
+            return redirect(next_page)
+        else:
+            # Login failed
+            messages.error(request, 'Invalid username or password.')
+            return render(request, self.template_name, {
+                'username': username  # Pre-fill username field
+            })
+        
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You have been successfully logged out.')
+        return redirect('login')
+    
+    def post(self, request):  # For POST requests (more secure)
+        logout(request)
+        messages.success(request, 'You have been successfully logged out.')
+        return redirect('login')
+
+    
+
+        
 
 
     
