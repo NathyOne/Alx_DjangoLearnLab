786c0051c4cdb385147eda6165f22d03bd2363e2
Nathaniel <nathanielabebe32@gmail.com>
2025-11-16 22:36:57 +0300

    members_view update

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', views.LogoutView.as_view(template_name="logout.html"), name='logout'),
-    path('admin-view', views.admin_view,name= 'admin' ),
-    path('librarian-view', views.librarian_view ,name= 'librarian' ),
-    path('member-view', views.member_view ,name= 'member' )
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 
 ]
 
