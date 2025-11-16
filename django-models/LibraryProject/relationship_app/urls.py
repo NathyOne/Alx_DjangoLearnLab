d1b79543cd14150400cd03d5e3daacf472ddc9c5
Nathaniel <nathanielabebe32@gmail.com>
2025-11-16 03:38:52 +0300

    small fix

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 from . import views
-
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('', views.lists_all_books, name="index"),
-    path('library/books', views.DetailViewOfLibrary.as_view(), name=" books_in_library"),
+    path('', views.lists_books, name="index"),
+    path('library/books', views.LibraryDetailView.as_view(), name=" books_in_library"),
 ]
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book, Author, Librarian, Library
 /from .models import Library
 from django.views.generic.detail import DetailView
 # Create your views here.
-def lists_all_books(request):
+def lists_books(request):
     book_list = Book.objects.all()
 
     context  = {
@@ -14,7 +14,7 @@ def lists_all_books(request):
 
 #Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
 # Utilize Django’s ListView or DetailView to structure this class-based view.
-class DetailViewOfLibrary(DetailView):
+class LibraryDetailView(DetailView):
     # all_books_in_the_Library = Library.objects.get(name="main library").books.all()
     all_books_in_library = Book.objects.filter(library__name="Main Library")
     template_name =  "relationship_app/library_detail.html"
