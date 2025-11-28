0a9c8942bad915dceb3dc71a03ac95bda1effe8e
Nathaniel <nathanielabebe32@gmail.com>
2025-11-28 15:03:43 +0300

    registered a custom user to admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
 # Register your models here.
 
-
+admin.register(CustomUser)
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display=( "title", "author", "publication_year")
