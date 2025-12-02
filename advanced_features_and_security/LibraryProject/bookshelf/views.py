b648bb0c0ae96b5a0df1261183a2f48d0ae486dd
Nathaniel <nathanielabebe32@gmail.com>
2025-12-02 09:46:44 +0300

    using defined permissions in view to the model

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,9 +3,10 @@ from django.views import View
 from .models import Book
 # Create your views here.
 from django.contrib.auth.decorators import permission_required
+
 @permission_required('can_view', raise_exception=True)
 class ViewBooks(View):
-    book_lists = Book.objects.all()
+    books = Book.objects.all()
     context = {
-        "book_lists":book_lists
+        "books":books
     }
