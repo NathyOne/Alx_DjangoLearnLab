8ff135697b6ab3e2270b61628ea35042460fe315
Nathaniel <nathanielabebe32@gmail.com>
2025-12-18 11:23:42 +0300

    model added

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework' ,
+    'rest_framework',
     'api'
 ]
 
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Books',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'root',
     }
 }
 
